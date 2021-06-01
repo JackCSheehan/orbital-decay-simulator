75dbf1aecad62b871856aec752a324ce5d1ff68b
jackcsheehan <jackcsheehan@users.noreply.github.com>
2021-06-01 12:20:26 -0500

    Created function to calculate density above 86 km

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # orbital-decay-simulator
 An interactive web app that allows for approximating and visualizing a spacecraft's orbital decay and reentry.
+
+This tool is currently in development. Expect more updates to this codebase and to this README in the near future.
+
+## Tech Stack
+#### [Streamlit](https://streamlit.io/)
+Streamlit is a front-end framework designed for data scientists and machine learning developers to quickly add interfaces to their projects. Streamlit is used for the entire front-end of this simulator.
+
+## References
+#### 1976 U.S. Standard Atmosphere Model
+- [Original model specification from NOAA](https://www.ngdc.noaa.gov/stp/space-weather/online-publications/miscellaneous/us-standard-atmosphere-1976/us-standard-atmosphere_st76-1562_noaa.pdf)
+- [Examples of constant derivations and curve fitting for densities above 86 km from Robert A. Braeunig](www.braeunig.us/space/atmmodel.htm)
--- src/atmosphere.py
+++ src/atmosphere.py
@@ -34,6 +34,21 @@ _REF_MOL_SCALE_TEMP = [288.15, 216.65, 216.65, 228.65, 270.65, 270.65, 214.65, 1
 # Standard Atmosphere paper. All pressures are in Pa
 _REF_PRESSURE = [101325, 22632.06, 5474.89, 868.02, 110.91, 66.94, 3.96]
 
+# Coefficients for a polynomial needed for solve for the density above 86 km.
+# Coefficients come from Robert Braeunig: www.braeunig.us/space/atmmodel.htm
+_DENSITY_COEFFICIENTS = [
+	(0, -3.322622e-6, 9.111460e-4, -0.2609971, 5.944694),
+	(0, 2.873405e-5, -0.008492037, 0.6541179, -23.62010),
+	(-1.240774e-5, 0.005162063, -0.8048342, 55.55996, -1443.338),
+	(0, -8.854164e-5, 0.03373254, -4.390837, 176.5294),
+	(3.661771e-7, -2.154344e-04, 0.04809214, -4.884744, 172.3597),
+	(1.906032e-8, -1.527799e-05, 0.004724294, -0.6992340, 20.50921),
+	(1.199282e-9, -1.451051e-6, 6.910474e-4, -0.1736220, -5.321644),
+	(1.140564e-10, -2.130756e-7, 1.570762e-4, -0.07029296, -12.89844),
+	(8.105631e-12, -2.358417e-9, -2.635110e-6, -0.01562608, -20.02246),
+	(-3.701195e-12, -8.608611e-9, 5.118829e-5, -0.06600998, -6.137674)
+]
+
 # Raises exception when an input is out of bounds of the Standard Atmosphere height range
 def _checkInStandardAtmosphereRange(z):
 	if z < 0 or z > 1000:
@@ -44,10 +59,10 @@ def _checkBelow86(z):
 	if z < 0 or z > 86:
 		raise Exception("Geometric height must be between 0 and 86 km")
 
-# Raises exception when an input is not within the 86 to 1000 km range
+# Raises exception when an input is not within the 86 (non-inclusive) to 1000 km range
 def _checkAbove86(z):
-	if z < 86 or z > 1000:
-		raise Exception("Geometric height must be between 86 and 1000 km")
+	if z <= 86 or z > 1000:
+		raise Exception("Geometric height must be between 87 and 1000 km")
 
 # Converts the given geometric height in km to geopotential km'
 def _geometricToGeopotentialHeight(z):
@@ -55,9 +70,9 @@ def _geometricToGeopotentialHeight(z):
 
 	return (_R0 * z) / (_R0 + z)
 
-# Converts the given geometric height to a subscript between 0 and 7 as defined in Table 4 of the U.S.
+# Converts the given geometric height to a subscript between 0 and 6 as defined in Table 4 of the U.S.
 # Standard Atmosphere paper
-def _heightToSubscript(z):
+def _heightToSubscriptBelow86(z):
 	_checkBelow86(z)
 
 	# Convert to km'
@@ -79,10 +94,37 @@ def _heightToSubscript(z):
 	elif (h > 71 and h <= 84.852) or math.isclose(h, 84.852, rel_tol = 1e-6):
 		return 6
 
+# Converts the given geometric height to a subscript between 0 and 9 line up with altitude ranges
+# in Table 8 of Robert Braeunig's page on atmospheric models
+def _heightToSubscriptAbove86(z):
+	_checkAbove86(z)
+	
+	# Check which altitude range the given height falls into
+	if z > 86 and z <= 91:
+		return 0
+	elif z > 91 and z <= 100:
+		return 1
+	elif z > 100 and z <= 110:
+		return 2
+	elif z > 110 and z <= 120:
+		return 3
+	elif z > 120 and z <= 150:
+		return 4
+	elif z > 150 and z <= 200:
+		return 5
+	elif z > 200 and z <= 300:
+		return 6
+	elif z > 300 and z <= 500:
+		return 7
+	elif z > 500 and z <= 750:
+		return 8
+	elif z > 750 and z <= 1000:
+		return 9
+
 # Returns pressure at given geometric height. Raises exception if z is not between 0 to 86 km
 def _getPressureBelow86(z):
 	# Get subscript corresponding to height
-	subscript = _heightToSubscript(z)
+	subscript = _heightToSubscriptBelow86(z)
 
 	# Get geopotential height
 	h = _geometricToGeopotentialHeight(z)
@@ -107,7 +149,7 @@ def _getPressureBelow86(z):
 		# Calculate value notated in brackets in paper
 		bracketValue = referenceMolScaleTemp / (referenceMolScaleTemp + molScaleTempGradient * (h - referenceHeight))
 
-		return referencePressure * math.pow(bracketValue, exponent)
+		return referencePressure * (bracketValue**exponent)
 	else:
 		# Calculate value notated in brackets in paper
 		bracketValue = ((-_G0 * _M0) * (h - referenceHeight)) / (_R * referenceMolScaleTemp)
@@ -119,7 +161,7 @@ def _getMolScaleTempBelow86(z):
 	_checkBelow86(z)
 
 	# Get subscript corresponding to this altitude
-	subscript = _heightToSubscript(z)
+	subscript = _heightToSubscriptBelow86(z)
 
 	# Convert geometric to geopotential height
 	h = _geometricToGeopotentialHeight(z)
@@ -132,20 +174,32 @@ def _getDensityBelow86(z):
 	_checkBelow86(z)
 
 	# Get subscript at given altitude
-	subscript = _heightToSubscript(z)
+	subscript = _heightToSubscriptBelow86(z)
 
 	# Get pressure at given altitude. Convert so distance units match with other values
 	pressure = _getPressureBelow86(z) * 1000
 
 	return (pressure * _M0) / (_R * _getMolScaleTempBelow86(z))
 
-
-# Returns density at given geometric height. Raises exception if z is not between 86 to 1000 km
+# Returns density at given geometric height. Raises exception if z is not between 87 to 1000 km
 def _getDensityAbove86(z):
-	pass
+	_checkAbove86(z)
+
+	# Get subscript corresponding given altitude
+	subscript = _heightToSubscriptAbove86(z)
+
+	# Get coefficients corresponding to this altitude range
+	a, b, c, d, e = _DENSITY_COEFFICIENTS[subscript]
+
+	# Return polynomial calculated with correct coefficients
+	return math.exp(a * z**4 + b * z**3 + c * z**2 + d * z + e) * 1000000000
 
 # Returns density at given geometric height. Raises exception if z is not between 0 to 1000 km
 def getDensity(z):
-	pass
+	_checkInStandardAtmosphereRange(z)
 
-print(_getDensityBelow86(0))
+	# Check range and call appropriate function
+	if z <= 86:
+		return _getDensityBelow86(z)
+	else:
+		return _getDensityAbove86(z)
