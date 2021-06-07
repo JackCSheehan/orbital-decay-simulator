Correct direction of Earth's rotation in ground track

src/orbital_mechanics.py
@@ -178,7 +178,7 @@ def calculateInitialOrbitTrackCoords(a, p, i, startingLat, startingLon):
 			launchSiteEquivalentLon = angle
 
 	# Add correction to launch site to determine first full orbit after launch
-	lon -= np.abs(launchSiteEquivalentLon - startingLon) - nodalDisplacement
+	lon -= np.abs(launchSiteEquivalentLon - startingLon) + nodalDisplacement
 	
 	# Add lat and lon of launch site to mark it on the map
 	lat = np.append(lat, startingLat)